cf279108942585cfd359d47bb431b99bcbce0900
AzrizHaziq <azrizhaziq@gmail.com>
2019-02-01 15:05:33 +0800

    - fix when close sidebar, page title not shrink, - update UI for list of machine

--- packages/web/components/machine/machine.js
+++ packages/web/components/machine/machine.js
@@ -51,30 +51,42 @@ function Machine() {
         </Col>
       </Row>
 
-      <div className='pt-3'>
-        <h4 className='font-weight-bold'>Scan History</h4>
+      <div className='pt-4'>
+        <h3 className='font-weight-bold text-primary'>
+          <Icon
+            type='hdd'
+            className='pr-1'
+            theme='twoTone'
+            twoToneColor='#1890ff'
+          />
+          Scan History
+        </h3>
         <List
           className='demo-loadmore-list'
           loading={false}
           itemLayout='horizontal'
           dataSource={[1, 2, 3]}
           renderItem={item => (
-            <List.Item actions={[<a>edit</a>, <a>more</a>]}>
+            <List.Item
+              actions={[
+                <div>
+                  <Icon type='clock-circle' className='d-inline ml-auto pr-1' />
+                  <Tooltip title='last scanned at'>
+                    <h6 className='d-inline text-muted-50'>23/02/2019</h6>
+                  </Tooltip>
+                </div>,
+                <a className='mr-2'>edit</a>,
+                <a>more</a>,
+              ]}
+            >
               <Skeleton avatar title={false} loading={false} active>
                 <List.Item.Meta
                   title={
                     <div className='d-flex align-items-center'>
                       <h4 className='pr-3 text-primary mb-0'>Scan 1</h4>
-                      <Icon
-                        type='clock-circle'
-                        className='ml-auto pr-1'
-                      />
-                      <Tooltip title="last scanned at">
-                        <span className='text-muted-50'>23/02/2019</span>
-                      </Tooltip>,
                     </div>
                   }
-                  description='lorem ipsum dolor emmet...'
+                  description='lorem ipsum dolor emmet...asdasdasd asd ada sdasd asdasda dad aads'
                 />
               </Skeleton>
             </List.Item>
